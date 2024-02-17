829daf185ed9d7e23c832df8e94ae7ade60ff041
Morpa <andremorpa@gmail.com>
2024-02-17 17:26:34 +0000

    add universal html

--- lib/providers/page_provider.dart
+++ lib/providers/page_provider.dart
@@ -1,7 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:universal_html/html.dart' as html;
 
 class PageProvider extends ChangeNotifier {
-  final scrollController = PageController();
+  var scrollController = PageController();
+
+  final List<String> _pages = [
+    'home',
+    'about',
+    'pricing',
+    'contact',
+    'location',
+  ];
+
+  int _currentIndex = 0;
+
+  void createScrollController(String routeName) {
+    scrollController = PageController(initialPage: getPageIndex(routeName));
+
+    html.document.title = _pages[getPageIndex(routeName)].toUpperCase();
+
+    scrollController.addListener(() {
+      final index = (scrollController.page ?? 0).round();
+
+      if (index != _currentIndex) {
+        html.window.history.pushState(null, 'none', '#/${_pages[index]}');
+        _currentIndex = index;
+        html.document.title = _pages[index].toUpperCase();
+      }
+    });
+  }
+
+  int getPageIndex(String routeName) {
+    return !_pages.contains(routeName) ? 0 : _pages.indexOf(routeName);
+  }
 
   void goTo(int index) {
     scrollController.animateToPage(
--- lib/router/router_handlers.dart
+++ lib/router/router_handlers.dart
@@ -1,11 +1,17 @@
 import 'package:fluro/fluro.dart';
+import 'package:provider/provider.dart';
 
+import '../providers/page_provider.dart';
 import '../ui/pages/home_page.dart';
 
-final homeHandler = Handler(handlerFunc: (_, params) {
-  final page = params['page']?.first;
+final homeHandler = Handler(handlerFunc: (context, params) {
+  final page = params['page']!.first;
 
   if (page != '/') {
+    final pageProvider = Provider.of<PageProvider>(context!, listen: false);
+
+    pageProvider.createScrollController(page);
+
     return const HomePage();
   }
   return null;
